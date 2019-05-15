e899d8eaa20d67740d84720fbb81e8dac88690a1
Jeff Henrikson <jeff.henrikson@47deg.com>
2019-05-15 11:24:01 -0700

    indexSpec: implement: test for diagnosis of TableJanitor cleanup issue.  Exact repro WIP

--- src/test/scala/repos/IndexSpec.scala
+++ src/test/scala/repos/IndexSpec.scala
@@ -3,18 +3,19 @@ package repos
 import java.util.UUID
 
 import org.scalatest.{MustMatchers, OptionValues}
-import repos.inmem.InMemDb
-import repos.testutils.{FooRepo, FooId, TestUtils}
-import scala.concurrent.ExecutionContext.Implicits.global
+import repos.testutils.{FooId, FooRepo, TestUtils}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import TestUtils.await
-import TestUtils.awaitStream
+import repos.jdbc.JdbcDb
+import slick.model.QualifiedName
+
+import scala.concurrent.Future
 
 
 class IndexSpec extends org.scalatest.fixture.FlatSpec with MustMatchers with OptionValues {
   type FixtureParam = Database
 
-  // Runs each tests against JDbcDb and InMemDb
   def withFixture(test: OneArgTest) = {
     val jdb = TestUtils.makeH2DB()
     val JdbcDb = TestUtils.makeH2JdbcDb(jdb)
@@ -26,19 +27,46 @@ class IndexSpec extends org.scalatest.fixture.FlatSpec with MustMatchers with Op
       jdb.shutdown
     }
   }
+  class FooTable(tableName:String) extends slick.model.Table(QualifiedName(tableName), Seq(), None, Seq(), Seq(), Set()) {
+  }
 
-  "partial indexes" should "work" in {
+  // TODO: remove blocking
+  def sizeOfTable(db:JdbcDb, nameOfTable:String) : Int = {
+    {
+      val s = db.asInstanceOf[JdbcDb].db.source.createConnection().createStatement()
+      val sql = s"select count(*) as a from ${nameOfTable}"
+      s.execute( sql)
+      val rs = s.getResultSet()
+      rs.next()
+      val num = rs.getInt("a")
+      // useful for debugging:
+      //println(s"sql=${sql}\ncount=${num}")
+      num
+    }
+  }
+
+  "partial indexes" should "have records cleaned up" in {
     db =>
       val id1 = FooId(UUID.randomUUID())
       val id2 = FooId(UUID.randomUUID())
       val id3 = FooId(UUID.randomUUID())
       val id4 = FooId(UUID.randomUUID())
       val id5 = FooId(UUID.randomUUID())
+      val id6 = FooId(UUID.randomUUID())
       await(db.run(FooRepo.insert(id1, "8")))
       await(db.run(FooRepo.insert(id2, "14")))
       await(db.run(FooRepo.insert(id3, "275")))
       await(db.run(FooRepo.insert(id4, "35")))
       await(db.run(FooRepo.insert(id5, "ababa")))
-      await(db.run(FooRepo.lengthIndex.allMatching(0))) must be(Seq())
+      await(db.run(FooRepo.insert(id6, "")))
+
+      val nameOfFullTable = FooRepo.name
+      val nameOfIndex =
+        db.asInstanceOf[JdbcDb].innerIndex(
+          SecondaryIndex(FooRepo,FooRepo.firstCharIndex.name,FooRepo.firstCharIndex.projection)
+        ).ix3TableName
+      val countOfFullTable = sizeOfTable(db.asInstanceOf[JdbcDb], nameOfFullTable)
+      val countOfIndexTable = sizeOfTable(db.asInstanceOf[JdbcDb], nameOfIndex)
+      countOfIndexTable must be < countOfFullTable
   }
 }
